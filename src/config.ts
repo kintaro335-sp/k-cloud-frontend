b77efe59d4a62ddebbd73d08b214a2aac37dae3d
kintaro335-sp <kintaroponce.uni@gmail.com>
2023-01-12 23:48:11 -0600

    agregado forma diferente de configurar el cliente

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,8 @@
-export const apiUrl = import.meta.env.VITE_API_URL;
+const url = import.meta.env.VITE_API_URL as string;
+const autoConfig = import.meta.env.VITE_API_AUTO as boolean;
+const port = import.meta.env.VITE_API_PORT as string;
+const uPort = `:${port}`;
+
+const noPortUrl = window.origin.split(/:[0-9]/)[0];
+
+export const apiUrl = autoConfig ? `${noPortUrl}${uPort}` : url;

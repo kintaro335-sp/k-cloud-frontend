49c21c3aeebe1dc880a389b8569cb017a4df0644
kintaro335-sp <kintaroponce.uni@gmail.com>
2023-02-04 14:08:54 -0600

    agregado de sistema de upload file

--- src/contexts/FileUploadContext.tsx
+++ src/contexts/FileUploadContext.tsx
@@ -2,7 +2,7 @@ import React, { createContext, useEffect } from 'react';
 import { useSelector, getState } from '../redux/store';
 import { addFile, onWriteBlob, setInitializedFile } from '../redux/slices/fileUploader';
 import { initializeFileAPI, writeBlobAPI, closeFileAPI } from '../api/files';
-import { getBestSize, getNumberBlobs } from '../utils/files';
+import { getBestSize, getNumberBlobs, BLOB_SIZE, getBase64File } from '../utils/files';
 
 export const FileUploadContext = createContext({ uploadFile: (path: string, file: File | null) => {} });
 
@@ -15,11 +15,60 @@ export default function FileUploadC({ children }: { children: React.ReactNode })
     addFile(path, file);
   };
 
+  const getFilesState = () => getState().files;
+
+  const initializeFile = async (path: string) => {
+    const files = getFilesState();
+    const fileM = files.files[path];
+    if (fileM === null) return;
+    if (fileM === undefined) return;
+    await initializeFileAPI(path, fileM.size, access_token);
+    setInitializedFile(path);
+    return;
+  };
+
+  const sendBlob = async (path: string, position: number, size: number) => {
+    const files = getFilesState();
+    const fileM = files.files[path];
+    if (fileM === null) return;
+    if (fileM === undefined) return;
+    const blob = fileM.file.slice(position, position + size);
+    const blobBase64 = getBase64File(blob);
+    if (typeof blobBase64 === 'string') {
+      await writeBlobAPI(path, position, blobBase64, access_token);
+      onWriteBlob(path, position, size);
+    }
+  };
+
+  const sendBlobs = async (path: string): Promise<void> => {
+    const files = getFilesState();
+    const fileM = files.files[path];
+    if (fileM === null) return;
+    if (fileM === undefined) return;
+    const blobs = getNumberBlobs(fileM.size);
+    for (let i = 0; i < blobs; i++) {
+      const position = BLOB_SIZE * i;
+      await sendBlob(path, position, BLOB_SIZE);
+    }
+    return new Promise((res) => {
+      res();
+    });
+  };
+
+  const uploadFileStart = async (path: string) => {
+    await initializeFile(path);
+    await sendBlobs(path);
+  };
+
   useEffect(() => {
-    console.log(filesDir);
-    filesDir.forEach((s) => {
-      setInitializedFile(s);
-      console.log(getState().files.files[s]);
+    filesDir.forEach((dir) => {
+      const files = getFilesState();
+      const fileM = files.files[dir];
+      if (fileM === null) return;
+      if (fileM === undefined) return;
+      if (!fileM.inicializado) {
+        uploadFileStart(dir);
+      }
     });
   }, [filesDir]);
 

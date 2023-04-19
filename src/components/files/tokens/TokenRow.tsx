af97b16dd1ff0d8c9999b168c0a373c383dd20ea
kintaro335-sp <kintaroponce.uni@gmail.com>
2023-04-18 20:32:21 -0600

    agregado link al token

--- src/components/files/tokens/TokenRow.tsx
+++ src/components/files/tokens/TokenRow.tsx
@@ -1,4 +1,4 @@
-import { TableRow, TableCell } from '@mui/material';
+import { TableRow, TableCell, Typography, Link } from '@mui/material';
 import TokenActions from './TokenActions';
 import { TokenElement } from '../../../@types/sharedfiles';
 import moment from 'moment';
@@ -10,7 +10,13 @@ export default function TokenRow({ token }: TokenRowProps) {
   const { id, expires, expire } = token;
   return (
     <TableRow>
-      <TableCell>{id}</TableCell>
+      <TableCell>
+        <Typography variant="subtitle1">
+          <Link target="_blank" href={`${window.origin}/shared-files/id/${id}`}>
+            {id}
+          </Link>
+        </Typography>
+      </TableCell>
       <TableCell>{expire ? 'si' : 'no'}</TableCell>
       <TableCell>{expire ? moment(expires).format('DD-MM-YYYY hh:mm a') : '-'}</TableCell>
       <TableCell>

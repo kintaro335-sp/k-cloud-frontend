2bf2fe44f65f664cffb257db9496088c3863ca56
kintaro335-sp <kintaroponce.uni@gmail.com>
2023-12-12 12:20:46 -0600

    corregir un problema de configuracion

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,9 @@ const url = import.meta.env.VITE_API_URL as string;
 const autoConfig = import.meta.env.VITE_API_AUTO;
 const port = import.meta.env.VITE_API_PORT as string;
 const uPort = `:${port}`;
-
-const noPortUrl = window.origin.split(/:[0-9]/)[0];
-const wsUrlR = noPortUrl.split('://')[1]
-
 const rawAuto = autoConfig === '1';
+const noPortUrl = rawAuto ? window.origin.split(/:[0-9]/)[0] : url.split(/:[0-9]/)[0];
+const wsUrlR = noPortUrl.split('://')[1];
+
 export const apiUrl = rawAuto ? `${noPortUrl}${uPort}` : url;
-export const wsUrl = `ws://${wsUrlR}:5001`
+export const wsUrl = `ws://${wsUrlR}:5001`;

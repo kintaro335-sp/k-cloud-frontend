4512d36a7baee97df889085e12be7c1c3bb9bd2f
kintaro335-sp <kintaroponce.uni@gmail.com>
2024-01-09 23:31:23 -0600

    feat: ocultar el usuario que es dueno

--- src/components/dashboard/accounts/UserItem.tsx
+++ src/components/dashboard/accounts/UserItem.tsx
@@ -1,18 +1,23 @@
 import { TableRow, TableCell } from '@mui/material';
 import MenuUser from './menu';
-import { useSelector } from '../../../redux/store'
+import { useSelector } from '../../../redux/store';
 // types
 import { User } from '../../../@types/admin';
 
 export default function UserItem({ user }: { user: User }) {
-  const  { owner } = useSelector((state) => state.admin);
+  const { owner } = useSelector((state) => state.admin);
   const { id, username, admin } = user;
+
+  if (id === owner) return <></>;
+
   return (
     <TableRow>
       <TableCell>
         <MenuUser user={user} />
       </TableCell>
-      <TableCell>{username} {owner === id && '(owner)'}</TableCell>
+      <TableCell>
+        {username} {owner === id && '(owner)'}
+      </TableCell>
       <TableCell>{admin ? 'si' : 'no'}</TableCell>
     </TableRow>
   );

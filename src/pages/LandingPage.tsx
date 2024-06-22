c877ee8bfabd9a0508c0f198f7ec516e5d6d1449
kintaro335-sp <kintaroponce.uni@gmail.com>
2024-06-22 15:55:20 -0600

    pulir el setup de el primer usuario

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,31 @@
-import { Box, Typography } from '@mui/material';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+// components
+import { Box, Typography, Button, Stack } from '@mui/material';
+// api
+import { getIsConfigured } from '../api/setup';
 
 export default function LandingPage() {
+  const [configured, setConfigured] = useState(false);
+
+  useEffect(() => {
+    getIsConfigured().then((result) => {
+      setConfigured(result.configured);
+    });
+  }, []);
+
   return (
-    <Box>
-      <Typography variant="h3" sx={{ color: 'text.secondary', textAlign: 'center' }}>
-        K Cloud
-      </Typography>
+    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <Stack>
+        <Typography variant="h3" sx={{ color: 'text.secondary', textAlign: 'center' }}>
+          K Cloud
+        </Typography>
+        {!configured && (
+          <Button component={Link} to="/setup" variant="contained" color="primary">
+            Crear primer usuario
+          </Button>
+        )}
+      </Stack>
     </Box>
   );
 }

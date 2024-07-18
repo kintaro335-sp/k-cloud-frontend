c4f4f406cdb98b8ea0732e888cc3ece90e6723cd
kintaro335-sp <kintaroponce.uni@gmail.com>
2024-07-17 18:32:52 -0600

    cambios en como se configura el cliente

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,19 @@
 const url = import.meta.env.VITE_API_URL as string;
+const url_ws = import.meta.env.VITE_API_URL_WS as string;
+const api_prefix = import.meta.env.VITE_API_PREFIX as string || '';
+const ws_prefix = import.meta.env.VITE_API_WS_PREFIX as string || '';
 const autoConfig = import.meta.env.VITE_API_AUTO;
-const port = import.meta.env.VITE_API_PORT as string;
-const uPort = `:${port}`;
-const rawAuto = autoConfig === '1';
-const noPortUrl = rawAuto ? window.origin.split(/:[0-9]/)[0] : url.split(/:[0-9]/)[0];
-const wsUrlR = noPortUrl.split('://')[1];
+const port = import.meta.env.VITE_API_PORT as string || '';
+const port_ws = import.meta.env.VITE_API_PORT_WS as string || '';
+const rawAuto = autoConfig === '1'; // 0 = false, 1 = true
 
-export const apiUrl = rawAuto ? `${noPortUrl}${uPort}` : url;
-export const wsUrl = `ws://${wsUrlR}:5001`;
+const cPort = port ? `:${port}`: '';
+const cPortWS = port_ws ? `:${port_ws}` : '';
+
+const noPortUrl = rawAuto ? window.origin.split(/:[0-9]/)[0] : url;
+const noPortwsUrlR =  rawAuto ? window.origin.split(/:[0-9]/)[0] : url_ws;
+
+export const apiUrl = rawAuto ? `${noPortUrl}${cPort}${api_prefix}` : url;
+export const wsUrl = rawAuto ? `${noPortwsUrlR}${cPortWS}${ws_prefix}` : url_ws;
 
 export const version = 'v1.0.3';

d544943b2e748b5536c0ba6a7ef4e491f5106501
kintaro335-sp <kintaroponce.uni@gmail.com>
2025-01-12 20:10:12 -0600

    mejoras en UX y agregado el goto

--- src/components/files/FilesList.tsx
+++ src/components/files/FilesList.tsx
@@ -5,12 +5,13 @@
  */
 
 import { useEffect, useState, useMemo, useRef } from 'react';
-import { Box, Grid, Stack, Card, CardContent, useMediaQuery } from '@mui/material';
+import { Box, Grid, useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import Loading from '../../pages/Loading';
 import FileElement from './FileElement';
 // redux
 import { useSelector } from '../../redux/store';
+import { setStart, setShowQ } from '../../redux/slices/session';
 // types
 import { FileI } from '../../@types/files';
 
@@ -20,25 +21,23 @@ interface FilesListProps {
 
 export default function FilesList({ loading }: FilesListProps) {
   const theme = useTheme();
-  const { files, path } = useSelector((state) => state.session);
+  const { files, path, start, showQ } = useSelector((state) => state.session);
   const scrollElement = useRef<HTMLDivElement>(null);
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
-  const scrollLimit = isMobile ? 0.978 : 0.930;
-  const [start, setStart] = useState<number>(0);
-  const [showQ, setShowQ] = useState<number>(48);
+  const scrollLimit = isMobile ? 0.978 : 0.93;
   const handleShowMore = () => {
     if (files.length < showQ) return;
     if (showQ >= 96) {
       setShowQ(96);
       return;
     }
-    setShowQ((prev) => prev + 8);
+    setShowQ(showQ + 8);
   };
 
   const handleChangeStart = (direction: 'back' | 'go') => {
     if (direction === 'back') {
       if (start === 0) return;
-      setStart((st) => {
+      const onSetStartBack = (st: number) => {
         const scrollHeight = scrollElement.current?.scrollHeight as number;
         const multiplier = isMobile ? 0.975 : 0.925;
         scrollElement.current?.scroll({ top: scrollHeight * multiplier });
@@ -47,22 +46,24 @@ export default function FilesList({ loading }: FilesListProps) {
           return 0;
         }
         return newVal;
-      });
+      };
+      setStart(onSetStartBack(start));
     }
     if (direction === 'go' && showQ >= 96) {
-      setStart((st) => {
+      const onSetStartGo = (st: number) => {
         const scrollHeight = scrollElement.current?.scrollHeight as number;
         const isLastPage = st + 96 >= files.length;
         const newVal = st + 96;
         if (newVal > files.length - 96) {
           if (!isLastPage) {
-            scrollElement.current?.scroll({ top: scrollHeight * 0.0001 });
+            scrollElement.current?.scroll({ top: scrollHeight * 0.0003 });
           }
           return files.length - 96;
         }
         scrollElement.current?.scroll({ top: scrollHeight * 0.0001 });
         return newVal;
-      });
+      };
+      setStart(onSetStartGo(start));
     }
   };
 
@@ -95,7 +96,11 @@ export default function FilesList({ loading }: FilesListProps) {
             }
           }}
         >
+          
           <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Box sx={{ padding: isMobile ? '5px' : '3px' }} />
+            </Grid>
             {filesMemo.slice(start, start + showQ).map((file: FileI, i) => (
               <Grid item key={file.name + i} xs={12} md={4} lg={3}>
                 <FileElement file={file} arrayIndex={start + i} />

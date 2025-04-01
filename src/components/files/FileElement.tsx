ba4e1cf85e88a2065d5844b41b312d44f6fe091c
kintaro335-sp <kintaroponce.uni@gmail.com>
2025-04-01 11:43:13 -0600

    update License

--- src/components/files/FileElement.tsx
+++ src/components/files/FileElement.tsx
@@ -1,6 +1,6 @@
 /*
  * k-cloud-frontend
- * Copyright(c) 2022 Kintaro Ponce
+ * Copyright(c) Kintaro Ponce
  * MIT Licensed
  */
 import { useEffect, useRef } from 'react';
--- src/components/search/FileElement.tsx
+++ src/components/search/FileElement.tsx
@@ -1,6 +1,6 @@
 /*
  * k-cloud-frontend
- * Copyright(c) 2022-2024 Kintaro Ponce
+ * Copyright(c)  Kintaro Ponce
  * MIT Licensed
  */
 

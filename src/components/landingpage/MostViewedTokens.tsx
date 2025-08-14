59a752bb6cc13cfffd466ff0832470f71263606d
kintaro335-sp <kintaroponce.uni@gmail.com>
2025-08-14 12:33:05 -0600

    agregado loading en landing page de tokens

--- src/components/landingpage/MostViewedTokens.tsx
+++ src/components/landingpage/MostViewedTokens.tsx
@@ -10,17 +10,21 @@ import { Box, Stack, Typography } from '@mui/material';
 import { Trans } from 'react-i18next';
 // components
 import TokenItem from './TokenItem';
+import { Loading } from '../../pages';
 // api
 import { getMostViewedTokens } from '../../api/sharedfiles';
 // types
 import { TokenElement } from '../../@types/sharedfiles';
 
 export default function MostViewedTokens() {
+  const [loading, setLoading] = useState(true);
   const [tokens, setTokens] = useState<TokenElement[]>([]);
 
   useEffect(() => {
+    setLoading(true);
     getMostViewedTokens().then((res) => {
       setTokens(res);
+      setLoading(false);
     });
   }, []);
 
@@ -30,9 +34,8 @@ export default function MostViewedTokens() {
         <Trans i18nKey="pages.home.most_viewed">Tokens con mas vistas</Trans>
       </Typography>
       <Stack spacing={2} direction="row" sx={{ padding: '10px', maxWidth: '95vw', overflowX: 'auto' }}>
-        {tokens.map((token) => (
-          <TokenItem key={token.id} token={token} />
-        ))}
+        {loading && <Loading height="350px" width="90vw" />}
+        {!loading && tokens.map((token) => <TokenItem key={token.id} token={token} />)}
       </Stack>
     </Box>
   );
